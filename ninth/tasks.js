86be78910061be10ea14dae9fabe95193494a482
alexviarhey <lexa.vergey@gmail.com>
2022-05-14 18:05:16 +0300

    update ninth

--- ninth/tasks.js
+++ ninth/tasks.js
@@ -12,96 +12,102 @@
 // let user = {
 //     name: 'My name'
 // };
+
 // [user.name, user.surname] = "Name Surname".split(' ')
+
 // console.log(user.name)
 
 
-
-// let {a, b, c} = {a: 1, b: 2, c: 3}
-
-
-// const sum = (...arg) => {
-//
-// }
-//
-// console.log(sum()) //0
-// console.log(sum(1)) //1
-// console.log(sum(1, 2)) //3
-// console.log(sum(1, 2, 3)) //6
-
-
 // let num = 'a'
-//
+
 // const logNum = () => {
 //     console.log(num)
 // }
-//
+
 // num = 'b'
-//
-// logNum()
+
+// logNum.bind({ num: 'c' })()
 
 
 // function User(name) {
+//     this.name = "default"
 //     this.sayHi = () => {
 //         console.log(name)
 //     }
 // }
-//
+
 // const user = new User("John")
-//
+
 // user.sayHi()
 
 
-// const arr = []
-// console.log(arr.__proto__.__proto__)
 
-// const a = () => {}
-// console.log(a.__proto__.__proto__.constructor === Object)
+// console.log(
+//     [].__proto__.__proto__ === null
+// )
+
+
+// console.log(
+//     (() => { }).__proto__.__proto__ === Object.prototype
+// )
 
 
 // const user = {
-//     sleep () {
+//     sleep() {
 //         this.isSleeping = true
 //     }
 // }
-//
+
 // const john = {
 //     __proto__: user
 // }
-//
+
 // john.sleep()
-//
+
 // console.log(john.isSleeping)
 
 
+// function User() { }
 
-// function User() {}
-//
-// const user = new User()// _proto_ -> { constructor }
-//
-// User.prototype = {
-//
-// }
-//
-//
+// User.prototype = {}
+
+// const user = new User()
+
 // console.log(user.constructor === User)
 
 
+
 // const a = {
-//
+//     name: 'a'
 // }
-//
+
 // const b = {
-//     hi: () => {
-//         console.log(this)
-//     },
-//
-//     hi2 () {
+//     name: 'b',
+
+//     hi() {
 //         (() => {
-//             console.log(this)
+//             console.log(this.name)
 //         }).call(a)
 //     }
 // }
-//
+
 // b.hi()
-// b.hi2()
+
+
+
+// const count = 0;
+
+// const counterCreator = () => {
+//     return () => {
+//         count += 1
+//     }
+// }
+
+// const counter = counterCreator()
+// const counter2 = counterCreator()
+
+// counter()
+// counter()
+// counter2()
+
+// console.log(count)
